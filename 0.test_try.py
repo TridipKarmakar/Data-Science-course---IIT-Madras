563c845d08dd03b7c63446a90166490e3a664768
Tridip Karmakar <karmakartridip97@gmail.com>
2024-10-24 22:11:42 +0530

    add GrPA 2 - Operations on list and set - GRADED

--- 0.test_try.py
+++ 0.test_try.py
@@ -1,3 +1,8 @@
-int_iterable = [1,2,3,45,6,7,8,9]
-maxi_int_interable  = max([1,2,3,45,6,7,8,9])
-print(maxi_int_interable) 
+some_iterable = [1,2,3,45,6,7,8,9]
+another_iterable = ("tridip", "karmakar") 
+yet_another_iterable = ("misti","das")
+
+all_concat = some_iterable + list(another_iterable) + list(yet_another_iterable)
+str_instamces = ["tridip", "karmakar","misti","das"]
+join_iterable = "_".join(sorted(str_instamces)) 
+print(join_iterable)
