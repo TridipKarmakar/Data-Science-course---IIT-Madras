ec337d7657ef5e69bad39e0ee2951ff5d63f9c73
Tridip Karmakar <karmakartridip97@gmail.com>
2024-10-26 01:01:59 +0530

    add week 2 GRPA assingment

--- 0.test_try.py
+++
@@ -1,7 +0,0 @@
-words = ["moon","noon","dad", "dog", "cat", "madam"]
-palindroms_numbers = []
-for word in words :
-    if word == word[::-1] :
-        palindroms_numbers.append(word)
-print(palindroms_numbers)
-        
